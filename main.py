bc0460c582023445a83315aeb6d5e59da2ae0667
sahil231090 <sahil.231090@gmail.com>
2019-06-29 18:46:48 -0700

    First  set of files

---
+++ main.py
@@ -0,0 +1,448 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 10:29:52 2019
+
+@author: sahil
+"""
+
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as web
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.neural_network import MLPRegressor
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.wrappers.scikit_learn import KerasRegressor
+
+
+
+# Get the data
+return_period = 21
+
+
+stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
+
+stk_data = web.DataReader(stk_tickers, 'yahoo')
+ccy_data = web.DataReader(ccy_tickers, 'fred')
+idx_data = web.DataReader(idx_tickers, 'fred')
+
+Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+Y.name = Y.name[-1]+'_pred'
+
+X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+X1.columns = X1.columns.droplevel()
+X2 = np.log(ccy_data).diff(return_period)
+X3 = np.log(idx_data).diff(return_period)
+
+X4 = pd.concat([Y.diff(i) for i in [21, 63, 126,252]], axis=1).dropna()
+X4.columns = ['1M', '3M', '6M', '1Y']
+
+X = pd.concat([X1, X2, X3, X4], axis=1)
+
+data = pd.concat([Y, X], axis=1).dropna()
+Y = data.loc[:, Y.name]
+X = data.loc[:, X.columns]
+
+
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,
+test_size=validation_size, random_state=seed)
+
+
+# Test options and evaluation metric
+num_folds = 10
+seed = 7
+scoring = 'neg_mean_squared_error'
+
+
+
+def simple_nn_model(nn_shape=None):
+    input_dim = X.shape[1]
+    if nn_shape is None:
+        # By default we add only 1 layer = input dim
+        nn_shape = [input_dim]
+    # create model
+    model = Sequential()
+    for idx, layer_shape in enumerate(nn_shape):
+        if idx == 0:
+            model.add(Dense(layer_shape, input_dim=input_dim,
+                            kernel_initializer='normal',
+                            activation='relu'))
+        else:
+            model.add(Dense(layer_shape,
+                kernel_initializer='normal',
+                activation='relu'))
+    # Final output layer
+    model.add(Dense(1, kernel_initializer='normal'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+def simple_lstm_model(seq_len=1, lstm_shape=None):
+    input_dim = X.shape[1]
+    if lstm_shape is None:
+        # By default we add only 1 layer = input dim
+        lstm_shape = [input_dim]
+    # create model
+    model = Sequential()
+    for idx, layer_shape in enumerate(lstm_shape):
+        if idx == 0:
+            model.add(LSTM(layer_shape, input_shape=(seq_len,input_dim)))
+        else:
+            model.add(LSTM(layer_shape))
+    # Final output layer
+    model.add(Dense(1))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+
+def KerasNNRegressor(nn_shape = None, epochs=100, batch_size=5, verbose=0):
+    return KerasRegressor(build_fn=simple_nn_model, epochs=epochs,
+                          batch_size=batch_size, verbose=verbose,
+                          nn_shape=nn_shape)
+
+
+class KerasLSTMRegressor(KerasRegressor):
+    def __init__(self, seq_len=1, lstm_shape = None, epochs=100, batch_size=5, verbose=0,
+                 build_fn=simple_lstm_model):
+        self.lstm_shape = lstm_shape
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.seq_len = seq_len
+        super().__init__(build_fn=simple_lstm_model,
+             epochs=epochs, batch_size=batch_size, verbose=verbose,
+             lstm_shape=lstm_shape, seq_len = seq_len)
+    
+    def fit(self, X, Y):
+        X_3d = np.zeros(X.shape[0]-self.seq_len+1, self.seq_len, X.shape[1])
+        for i in range(self.seq_len):
+            X_3d[:, i, :] = X[i:X.shape[0]-self.seq_len+1+i,:]
+        return super().fit(X_3d, Y)
+    
+    def predict(self, X):
+        X_3d = np.zeros(X.shape[0]-self.seq_len+1, self.seq_len, X.shape[1])
+        for i in range(self.seq_len):
+            X_3d[:, i, :] = X[i:X.shape[0]-self.seq_len+1+i,:]
+        return super().predict(X_3d)
+
+    
+
+
+models = []
+models.append(('LR', LinearRegression()))
+models.append(('LASSO', Lasso()))
+models.append(('EN', ElasticNet()))
+models.append(('KNN', KNeighborsRegressor()))
+models.append(('CART', DecisionTreeRegressor()))
+models.append(('SVR', SVR()))
+models.append(('MLP', MLPRegressor()))
+models.append(('RFR', RandomForestRegressor()))
+models.append(('GBR', GradientBoostingRegressor()))
+models.append(('ETR', ExtraTreesRegressor()))
+models.append(('ABR', AdaBoostRegressor()))
+models.append(('NNR', KerasNNRegressor()))
+models.append(('LSTMR', KerasLSTMRegressor()))
+
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+
+# 1. Grid search : LinearRegression
+'''
+fit_intercept : boolean, optional, default True
+    whether to calculate the intercept for this model. If set
+    to False, no intercept will be used in calculations
+    (e.g. data is expected to be already centered).
+'''
+param_grid = {'fit_intercept': [True, False]}
+model = LinearRegression()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 2. Grid search : Lasso
+'''
+alpha : float, optional
+    Constant that multiplies the L1 term. Defaults to 1.0.
+    ``alpha = 0`` is equivalent to an ordinary least square, solved
+    by the :class:`LinearRegression` object. For numerical
+    reasons, using ``alpha = 0`` with the ``Lasso`` object is not advised.
+    Given this, you should use the :class:`LinearRegression` object.
+''' 
+param_grid = {'alpha': [0.01, 0.1, 0.3, 0.7, 1, 1.5, 3, 5]}
+model = Lasso()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 3. Grid Search : ElasticNet
+'''
+alpha : float, optional
+    Constant that multiplies the penalty terms. Defaults to 1.0.
+    See the notes for the exact mathematical meaning of this
+    parameter.``alpha = 0`` is equivalent to an ordinary least square,
+    solved by the :class:`LinearRegression` object. For numerical
+    reasons, using ``alpha = 0`` with the ``Lasso`` object is not advised.
+    Given this, you should use the :class:`LinearRegression` object.
+
+l1_ratio : float
+    The ElasticNet mixing parameter, with ``0 <= l1_ratio <= 1``. For
+    ``l1_ratio = 0`` the penalty is an L2 penalty. ``For l1_ratio = 1`` it
+    is an L1 penalty.  For ``0 < l1_ratio < 1``, the penalty is a
+    combination of L1 and L2.
+'''
+param_grid = {'alpha': [0.01, 0.1, 0.3, 0.7, 1, 1.5, 3, 5],
+              'l1_ratio': [0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 0.99]}
+model = ElasticNet()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 4. Grid search : KNeighborsRegressor 
+'''
+n_neighbors : int, optional (default = 5)
+    Number of neighbors to use by default for :meth:`kneighbors` queries.
+'''
+param_grid = {'n_neighbors': [1,3,5,7,9,11,13,15,17,19,21]}
+model = KNeighborsRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 5. Grid search : DecisionTreeRegressor 
+'''
+min_samples_split : int, float, optional (default=2)
+    The minimum number of samples required to split an internal node:
+
+    - If int, then consider `min_samples_split` as the minimum number.
+    - If float, then `min_samples_split` is a percentage and
+      `ceil(min_samples_split * n_samples)` are the minimum
+      number of samples for each split.
+'''
+param_grid={'min_samples_split': [2,3,4,5,6,7,8,9,10]}
+model = DecisionTreeRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 6. Grid search : SVR 
+'''
+C : float, optional (default=1.0)
+    Penalty parameter C of the error term.
+
+epsilon : float, optional (default=0.1)
+     Epsilon in the epsilon-SVR model. It specifies the epsilon-tube
+     within which no penalty is associated in the training loss function
+     with points predicted within a distance epsilon from the actual
+     value.
+gamma : float, optional (default='auto')
+    Kernel coefficient for 'rbf', 'poly' and 'sigmoid'.
+    If gamma is 'auto' then 1/n_features will be used instead.
+'''
+param_grid={'C': [0.01, 0.03,0.1,0.3,1,3,10,30,100],
+            'gamma': [0.001, 0.01, 0.1, 1],
+            'epslion': [0.01, 0.1, 1]}
+model = SVR()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# 7. Grid search : MLPRegressor 
+'''
+hidden_layer_sizes : tuple, length = n_layers - 2, default (100,)
+    The ith element represents the number of neurons in the ith
+    hidden layer.
+'''
+param_grid={'hidden_layer_sizes': [(20,), (50,), (20,20), (20, 30, 20)]}
+model = MLPRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# 8. Grid search : RandomForestRegressor 
+'''
+n_estimators : integer, optional (default=10)
+    The number of trees in the forest.
+'''
+param_grid = {'n_estimators': [50,100,150,200,250,300,350,400]}
+model = RandomForestRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+
+# 9. Grid search : GradientBoostingRegressor 
+'''
+n_estimators:
+
+    The number of boosting stages to perform. Gradient boosting
+    is fairly robust to over-fitting so a large number usually
+    results in better performance.
+''' 
+param_grid = {'n_estimators': [50,100,150,200,250,300,350,400]}
+model = GradientBoostingRegressor(random_state=seed)
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# 10. Grid search : ExtraTreesRegressor 
+'''
+n_estimators : integer, optional (default=10)
+    The number of trees in the forest.
+''' 
+param_grid = {'n_estimators': [50,100,150,200,250,300,350,400]}
+model = ExtraTreesRegressor(random_state=seed)
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 11. Grid search : AdaBoostRegressor 
+'''
+n_estimators : integer, optional (default=50)
+    The maximum number of estimators at which boosting is terminated.
+    In case of perfect fit, the learning procedure is stopped early.
+
+learning_rate : float, optional (default=1.)
+    Learning rate shrinks the contribution of each regressor by
+    ``learning_rate``. There is a trade-off between ``learning_rate`` and
+    ``n_estimators``.
+''' 
+param_grid = {'n_estimators': [50,100,150,200,250,300,350,400],
+             'learning_rate': [1, 2, 3]}
+model = AdaBoostRegressor(random_state=seed)
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+# 12. Grid search : KerasNNRegressor 
+'''
+nn_shape : tuple, length = n_layers - 2, default (100,)
+    The ith element represents the number of neurons in the ith
+    hidden layer.
+''' 
+param_grid={'nn_shape': [(20,), (50,), (20,20), (20, 30, 20)]}
+model = KerasNNRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+
+
---
+++ test_LassoRegression.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 10:29:52 2019
+
+@author: sahil
+"""
+
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as web
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Lasso
+
+
+# Get the data
+def get_data():
+	return_period = 21
+
+
+
+	stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+	ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+	idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
+
+	stk_data = web.DataReader(stk_tickers, 'yahoo')
+	ccy_data = web.DataReader(ccy_tickers, 'fred')
+	idx_data = web.DataReader(idx_tickers, 'fred')
+
+	Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+	Y.name = Y.name[-1]+'_pred'
+
+	X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+	X1.columns = X1.columns.droplevel()
+	X2 = np.log(ccy_data).diff(return_period)
+	X3 = np.log(idx_data).diff(return_period)
+
+	X4 = pd.concat([Y.diff(i) for i in [21, 63, 126,252]], axis=1).dropna()
+	X4.columns = ['1M', '3M', '6M', '1Y']
+
+	X = pd.concat([X1, X2, X3, X4], axis=1)
+
+	data = pd.concat([Y, X], axis=1).dropna()
+	Y = data.loc[:, Y.name]
+	X = data.loc[:, X.columns]
+	return X, Y
+
+X, Y = get_data()
+
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,
+test_size=validation_size, random_state=seed)
+
+
+# Test options and evaluation metric
+num_folds = 10
+seed = 7
+scoring = 'neg_mean_squared_error'
+
+
+
+# Grid search : Lasso
+'''
+alpha : float, optional
+    Constant that multiplies the L1 term. Defaults to 1.0.
+    ``alpha = 0`` is equivalent to an ordinary least square, solved
+    by the :class:`LinearRegression` object. For numerical
+    reasons, using ``alpha = 0`` with the ``Lasso`` object is not advised.
+    Given this, you should use the :class:`LinearRegression` object.
+''' 
+param_grid = {'alpha': [0.01, 0.1, 0.3, 0.7, 1, 1.5, 3, 5]}
+model = Lasso()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
